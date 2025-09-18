1c1b48f9627509c4aa324a6aaa01ec84b71375d7
nikita <nikitajadhav4487@gmail.com>
2025-09-18 05:27:35 +0000

    new file added

---
+++ p1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	while(1)
+	{
+		printf("option\n1)add_begin\n2)print\n3)delete\n4)exit\n");
+
+	int op;
+	printf("enter the op\n");
+	scanf("%d",&op);
+switch(op)
+{
+	case 1: add_begin(&head);
+		braek;
+
+        case 2: print(head);
+		braek;
+
+
